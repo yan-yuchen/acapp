29028de7be62f705c0840fcaa0db916ffb3beec6
yuchen yan <y.c.yan@qq.com>
2021-11-04 21:33:06 +0800

    only show the background

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -10,13 +10,3 @@ class AcGameMenu{
         
     }
 }
-class AcGame {
-    constructor(id) {
-        this.id = id;
-        this.$ac_game = $('#' + id);   //将对象的id赋给ac_game
-        this.menu = new AcGameMenu(this);  //创建菜单对象，调用其构造函数
-
-    }
-
-}
-
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,14 +1,10 @@
 class AcGame {
-        constructor(id) {
-                    this.id = id;
-                    this.$ac_game = $('#' + id);
-                    this.menu = new AcGameMenu(this);
-                    this.playground = new AcGamePlayground(this);
+    constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);   //将对象的id赋给ac_game
+        this.menu = new AcGameMenu(this);  //创建菜单对象，调用其构造函数
 
-                    this.start();
-                }
+    }
 
-        start() {
-                }
 }
 
