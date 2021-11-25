d11bd20aa7a1d30b856c3b10e55f1d406bf364dd
yuchen yan <y.c.yan@qq.com>
2021-11-26 01:57:46 +0800

    update a little

--- game/models/player/player.py
+++ game/models/player/player.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.URLField(max_length=256, blank=True)
+    openid=models.CharField(default='',max_length=50,blank=True,null=True)
 
     def __str__(self):     #后台显示每个player的user
         return str(self.user)
